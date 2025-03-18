02fb3d2e0274ef39ed87306ea513a2198c96e2ef
shanmukh <josyulashanmukh@gmail.com>
2025-03-18 19:49:58 +0530

    add

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resources "aws_instances" "app_ec2"{
-    ami = "ami-tu8t789709"
+resource "aws_instance" "myec2" {
+    ami = "ami-04b4f1a9cf54c11d0"
     instance_type = "t2.micro"
-
+    
     tags = {
-        Name = "shanmukh"
+        Name = "ec2-created-from-terraform"
     }
 }
